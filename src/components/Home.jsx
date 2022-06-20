f3dc76ba1dff46c77d043a3931c11b0ba67a2793
Frédéric Ferreira <fred.69780@hotmail.fr>
2022-06-20 16:47:02 +0200

    upload

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import lottie from 'lottie-web';
 
 import laces from '../animation/data.json';
 
 function Home() {
-  const [showContent, setShowContent] = useState(false);
-
   useEffect(() => {
-    setTimeout(() => {
-      setShowContent(true);
-      lottie.loadAnimation({
-        container: document.getElementById('svg-container'),
-        animationData: laces,
-      });
-    }, 500);
+    lottie.loadAnimation({
+      container: document.getElementById('svg-container'),
+      animationData: laces,
+    });
   }, []);
 
   return (
     <div id="home">
       <h1>Hey!</h1>
-      {showContent && (
-        <>
-          <div id="svg-container" />
-          <h2>Time to tie your shoes ...</h2>
-        </>
-      )}
+      <div id="svg-container" />
+      <h2>Time to tie your shoes ...</h2>
       <Link to="/shop">
         <button type="button">Shop now</button>
       </Link>
